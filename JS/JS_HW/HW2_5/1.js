8007ff2c71a56e2d922db8ada8e3a8e262dcdd01
Daniil Zakharov <zdan14@mail.ru>
2024-05-09 17:38:54 +0300

    Добавил решение к 5 семинару

---
+++ JS/JS_HW/HW2_5/1.js
@@ -0,0 +1,28 @@
+const dataInfo = `
+[
+
+    {
+        "class": "img__container",
+        "url": "https://www.genevadrivingschool.ch/wp-content/uploads/2018/11/1-1.jpg",
+        "width": "250",
+        "height": "250",
+        "text": "Картинка №1"
+
+    },
+    {
+        "class": "img__container",
+        "url": "https://xn----7sbbblh9b0av4l.xn--j1amh/wp-content/uploads/2018/10/cifra_2_5-225x300.jpg",
+        "width": "300",
+        "height": "350",
+        "text": "Картинка №2"
+    },
+    {
+        "class": "img__container",
+        "url": "https://lerua.katalog-ceny.ru/image/cache/catalog/lerua2/cifra-3-duckanddog-malenkaya-70h65-mm-stal-cvet-chernij-15632764-200x200.jpg",
+        "width": "150",
+        "height": "300",
+        "text": "Картинка №3"
+    }
+
+]
+`;
